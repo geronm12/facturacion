c25881c034936ccb6307559b15152b5b7d61751f
geronimo <gerolpz01@gmail.com>
2020-07-25 21:08:05 -0300

    Aplicadas las configuraciones, falta agregar los foreign keys y aplicar la migration

--- Infraestructura/DataContext.cs
+++ Infraestructura/DataContext.cs
@@ -1,7 +1,10 @@
 ﻿using Facturacion.Conexion;
+using Facturacion.Dominio.Entities;
 
 namespace Infraestructura
 {
+    using Facturacion.Dominio.Entities;
+    using Facturacion.Dominio.MetaData;
     using Microsoft.EntityFrameworkCore;
     using System.IO.Compression;
     using System.Linq;
@@ -31,8 +34,26 @@ namespace Infraestructura
 
 
 
-
-
+            modelBuilder.ApplyConfiguration(new CajaMetaData());
+            modelBuilder.ApplyConfiguration(new CarritoMetaData());
+            modelBuilder.ApplyConfiguration(new CategoriaMetaData());
+            modelBuilder.ApplyConfiguration(new ClienteMetaData());
+            modelBuilder.ApplyConfiguration(new DetalleDeCajaMetaData());
+            modelBuilder.ApplyConfiguration(new DetalleFacturaMetaData());
+            modelBuilder.ApplyConfiguration(new EmpleadoMetaData());
+            modelBuilder.ApplyConfiguration(new FacturaCompraMetaData());
+            modelBuilder.ApplyConfiguration(new FacturaVentaMetaData());
+            modelBuilder.ApplyConfiguration(new FormaPagoMetaData());
+            modelBuilder.ApplyConfiguration(new ListaDePreciosMetaData());
+            modelBuilder.ApplyConfiguration(new MarcaMetaData());
+            modelBuilder.ApplyConfiguration(new MovimientoMetaData());
+            modelBuilder.ApplyConfiguration(new OrdenMetaData());
+            modelBuilder.ApplyConfiguration(new PrecioMetaData());
+            modelBuilder.ApplyConfiguration(new PrecioListaPrecioMetaData());
+            modelBuilder.ApplyConfiguration(new ProductoMetaData());
+            modelBuilder.ApplyConfiguration(new ProveedorMetaData());
+            modelBuilder.ApplyConfiguration(new UsuarioMetaData());
+           
             base.OnModelCreating(modelBuilder);
         }
 
@@ -48,6 +69,35 @@ namespace Infraestructura
   
             return base.SaveChangesAsync();
         }
+
+        public DbSet<Caja> Cajas {get; set;}
+        public DbSet<Carrito> Carritos {get; set;}
+        public DbSet<Categoria> Categorias  {get; set;}
+        public DbSet<Cliente>  Clientes {get; set;}
+        public DbSet<DetalleDeCaja> DetalleDeCajas{get; set;}
+        public DbSet<DetalleFactura> DetalleFacturas {get; set;}
+        public DbSet<Emplead> Empleados {get; set;}
+        public DbSet<FacturaVenta> FacturaVentas {get; set;}
+        public DbSet<FacturaCompra> FacturaCompras {get; set;}
+        public DbSet<FormaPago> FormaPagos {get; set;}
+
+        public DbSet<ListaDePrecios> ListaDePrecios {get; set;}
+        public DbSet<Marca> Marcas {get; set;}
+        public DbSet<Movimiento> Movimientos {get; set;}
+        public DbSet<Orden> Ordens { get; set;}
+        public DbSet<Precio> Precios { get; set;}
+        public DbSet<PrecioListaPrecio> PrecioListaPrecios { get; set;}
+        public DbSet<Producto> Productos { get; set;}
+        public DbSet<Proveedor> Proveedors { get; set;}
+        public DbSet<Usuario> Usuarios { get; set;}
+     
+
+
+
+
+
+
+
 
 
 
